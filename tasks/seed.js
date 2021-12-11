83c7c30d1b0785741ae42f9b417541d77169d4de
shraddha kumbhar <shraddhakumbhar17@gmail.com>
2021-12-11 18:25:32 -0500

    Committed changes regarding seed file

--- tasks/seed.js
+++ tasks/seed.js
@@ -43,35 +43,114 @@ const main = async() => {
     console.log(user1, "User 1 created successfully");
 
     const user2 = await customersData.createUser("Kate", "Ken", "kate@gmail.com", "katekk", " katekk123", "", "New Jersey", "New Jersey", "21");
-    console.log(user2, "User 1 created successfully");
+    console.log(user2, "User 2 created successfully");
 
     const user3 = await customersData.createUser("Tonny", "Keith", "tonny@gmail.com", "tonnykk", "tonnykeith@123", "", "California", "California", "23");
-    console.log(user3, "User 1 created successfully");
+    console.log(user3, "User 3 created successfully");
 
     const user4 = await customersData.createUser("Richard", "Endrew", "richard@gmail.com", "richardend", "richard@1234", "", "ohio", "ohio", "24");
-    console.log(user4, "User 1 created successfully");
+    console.log(user4, "User 4 created successfully");
 
     const user5 = await customersData.createUser("Samuel", "jage", "samuel@gmail.com", "samueljage", "samuel@12", "", "hoboken", "New Jersey", "20");
-    console.log(user5, "User 1 created successfully");
+    console.log(user5, "User 5 created successfully");
 
     const user6 = await customersData.createUser("Raymond", "wright", "raymond@gmail.com", "raymondwright", "raymond@w123", "", "texas", "texas", "26");
-    console.log(user6, "User 1 created successfully");
+    console.log(user6, "User 6 created successfully");
 
     const user7 = await customersData.createUser("nina", "dobrev", "nina@gmail.com", "ninadob", "ninadob@rev", "", "New York", "New York", "24");
-    console.log(user7, "User 1 created successfully");
+    console.log(user7, "User 7 created successfully");
 
     const user8 = await customersData.createUser("shraddha", "khair", "shraddha@gmail.com", "shraddhak", "shraddha123kh", "", "Edison", "New Jersey", "25");
-    console.log(user8, "User 1 created successfully");
+    console.log(user8, "User 8 created successfully");
 
     const user9 = await customersData.createUser("Savleen", "Kaur", "savleen@gmail.com", "savleenkaur", "savleenk@678kaur", "", "Nutley", "New Jersey", "24");
-    console.log(user9, "User 1 created successfully");
+    console.log(user9, "User 9 created successfully");
 
     const user10 = await customersData.createUser("Prateek", "Upereti", "prateek@gmail.com", "prateekup", "prateek@123", "", "Hoboken", "New Jersey", "24");
-    console.log(user10, "User 1 created successfully");
+    console.log(user10, "User 10 created successfully");
 
     const user11 = await customersData.createUser("Muzzaffar", "Turak", "muzzaffar@gmail.com", "muzzaffar", "muzzaffartu", "", "Boton", "Boston", "24");
-    console.log(user11, "User 1 created successfully");
+    console.log(user11, "User 11 created successfully");
 
+    const review11 = await reviewsData.create(DreamZ._id, user1._id, 'This is amazing', "9")
+    console.log(review11)
+    console.log('Reviews11 added successfully')
+
+    const review12 = await reviewsData.create(DreamZ._id, user1._id, 'This salon is not good', "4")
+    console.log(review12)
+    console.log('Reviews12 added successfully')
+
+    const review21 = await reviewsData.create(Telogen._id, user2._id, 'This salon is perfectly good', "9")
+    console.log(review21)
+    console.log('Reviews21 added successfully')
+
+    const review22 = await reviewsData.create(Telogen._id, user2._id, 'This salon smells good', "7")
+    console.log(review22)
+    console.log('Reviews22 added successfully')
+
+    const review31 = await reviewsData.create(Roger._id, user3._id, 'This salon looks amazing', "7")
+    console.log(review31)
+    console.log('Reviews31 added successfully')
+
+    const review32 = await reviewsData.create(Roger._id, user3._id, 'This salon services are not good', "2")
+    console.log(review32)
+    console.log('Reviews32 added successfully')
+
+    const review41 = await reviewsData.create(Bloom._id, user4._id, 'This salon looks amazing', "8")
+    console.log(review41)
+    console.log('Reviews41 added successfully')
+
+    const review42 = await reviewsData.create(Bloom._id, user4._id, 'This salon services are not worth', "2")
+    console.log(review42)
+    console.log('Reviews42 added successfully')
+
+    const review43 = await reviewsData.create(Bloom._id, user4._id, 'This salon smells bad', "3")
+    console.log(review43)
+    console.log('Reviews43 added successfully')
+
+    const review51 = await reviewsData.create(Aerea._id, user5._id, 'Very pleasant atmosphere, especially considering the difficult times we are all facing with the current pandemic.', "8")
+    console.log(review51)
+    console.log('Reviews51 added successfully')
+
+    const review52 = await reviewsData.create(Aerea._id, user5._id, 'Extremely clean area! Greeted very warmly upon my entrance and seated almost immediately.', "9")
+    console.log(review52)
+    console.log('Reviews52 added successfully')
+
+    const review61 = await reviewsData.create(Madison._id, user6._id, 'Absolutely cannot recommend this salon enough, every time I go I get consistent, quality service. Pricing is more than reasonable. ', "5")
+    console.log(review61)
+    console.log('Reviews61 added successfully')
+
+    const review62 = await reviewsData.create(Madison._id, user6._id, ' I had gotten an unfortunate haircut from another establishment and she spent the time fixing every detail of my hair until it was perfect. She’s so skilled and so sweet. Highly recommend!', "9")
+    console.log(review62)
+    console.log('Reviews62 added successfully')
+
+    const review71 = await reviewsData.create(Caru._id, user7._id, 'Great haircuts, fantastic services, love the atmosphere, nothing but a good afternoon.', "7")
+    console.log(review71)
+    console.log('Reviews71 added successfully')
+
+    const review72 = await reviewsData.create(Caru._id, user7._id, ' This is the only salon that I trust to properly deal with my hair. Their extensive knowledge and skill are truly unmatched.', "8")
+    console.log(review72)
+    console.log('Reviews72 added successfully')
+
+    const review81 = await reviewsData.create(Phoenix._id, user8._id, 'My first visit to this location was wonderful. Great vibe and friendly, helpful staff. Plus there is a bar !!', "7")
+    console.log(review81)
+    console.log('Reviews81 added successfully')
+
+    const review82 = await reviewsData.create(Phoenix._id, user8._id, ' My first visit to this location was wonderful. Great vibe and friendly, helpful staff. Plus there is a bar !!', "9")
+    console.log(review82)
+    console.log('Reviews82 added successfully')
+
+    const review91 = await reviewsData.create(Hudson._id, user9._id, 'Not good and recommended', "6")
+    console.log(review91)
+    console.log('Reviews91 added successfully')
+
+    const review92 = await reviewsData.create(Hudson._id, user9._id, ' Got a cut due to their equipments...Not at all recommended', "4")
+    console.log(review92)
+    console.log('Reviews92 added successfully')
+
+    const review93 = await reviewsData.create(Hudson._id, user9._id, 'Not punctual and service provided is not at all satisfactory', "4")
+    console.log(review93)
+    console.log('Reviews93 added successfully')
 
 
     // const skyLounge = await salonsData.create("The lounge", "http://www.skylounge.com", "Spa", "New York City, New York", "New York", " NJ", "08820", "40.732628", "-74.037628");
